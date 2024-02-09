0b81071935aa04d2fb375f0eadc457ff3dfb94ad
BP <66450457+bpowers24@users.noreply.github.com>
2024-02-09 09:52:02 -0800

    Create data_package_checks.R

---
+++ Data_Package_Validation/data_package_checks.R
@@ -0,0 +1,17 @@
+### data_package_checks.R ######################################################
+
+# Objective: 
+
+
+### User Inputs ################################################################
+
+
+
+
+### Prep Script ################################################################
+
+# load libraries
+library(tidyverse)
+library(rlog)
+library(fs) # for tree diagram
+library(clipr) # for copying to clipboard
