9004a26aa27e55f52860f7e46af80f25b7432713
BP <66450457+bpowers24@users.noreply.github.com>
2024-02-13 12:09:56 -0800

    check_files.R function v1 complete

--- Data_Package_Validation/functions/check_files.R
+++ Data_Package_Validation/functions/check_files.R
@@ -49,6 +49,14 @@ check_files <- function(directory) {
     failed_checks = data.frame()
   )
   
+  # initialize empty report
+  report <- data.frame(
+    pass_check = as.character(),
+    type = as.character(),
+    assessment = as.character(),
+    summary = as.character()
+  )
+  
   # initialize an empty df to store the failed checks
   failed_checks <- data.frame(
     pass_check = as.logical(),
@@ -195,43 +203,214 @@ check_files <- function(directory) {
     
   }
   
-  # add report to output list
-  data_checks$report <- folder_report
-  
   # add folder_checks to output list
   data_checks$data_checks$folders <- folder_checks
-  
-  # add failed_checks to output list
-  data_checks$failed_checks <- failed_checks
   
   log_info("Folder check complete.")
   
   
   ### File Checks ----
+  # file checks complete 3 steps for each check (spaces, special chrs, and extensions)
+    # 1. completes the checks for all 3 checks
+    # then assesses each check...
+    # 2. filters for failed checks and adds to failed_checks df
+    # 3. filters for failed checks and summarizes them in report
   
+  current_files <- list.files(path = current_directory, full.names = F, recursive = T)
   
+  log_info(paste0("Checking ", length(current_files), " file(s) for errors."))
   
+  # initialize empty df for file checks
+  file_checks <- data.frame(
+    pass_check = as.logical(),
+    type = as.character(),
+    item = as.character(),
+    assessment = as.character(),
+    source = as.character(),
+    notes = as.character()
+  )
   
+  for (i in 1:length(current_files)) {
+    
+    # get current folder path
+    current_folder_path <- current_files[i]
+    
+    # get current file
+    current_item <- basename(current_folder_path)
+    
+    # get associated folder
+    current_source <- dirname(current_folder_path)
+    
+    # check for spaces
+    has_spaces <- grepl(" ", current_item)
+    
+    # split out by character
+    split_chrs <- unlist(strsplit(current_item, ""))
+    
+    # check for special characters
+    has_special_chrs <- length(grep("[^a-zA-Z0-9_/\\.-]", split_chrs)) > 0 # chrs allowed: lowercase letter, uppercase letter, digit, underscore, forward slash, backslash, period, or hyphen.
+    
+    # get the special character values
+    if (has_special_chrs == FALSE) {
+      special_characters <- "none"
+    } else {
+      special_characters <- paste0("\"", paste(grep("[^a-zA-Z0-9_/\\.-]", split_chrs, value = TRUE), collapse = ""), "\"")
+    }
+    
+    # get the extension
+    current_extension <- tools::file_ext(current_item)
+    
+    # get list of invalid extensions
+    bad_extensions <- c("docx", "doc", "xlsx", "pptx", "ppt", # microsoft extensions
+                        ".m", ".fig", ".mat", ".mlx", ".p", ".mdl" # matlab extensions
+    )
+    # check for not approved extensions
+    non_proprietary <- !current_extension %in% bad_extensions
+    
+    # add row to checks
+    file_checks <- file_checks %>% 
+      # add row for spaces
+      add_row(pass_check = !has_spaces,
+              type = "file",
+              item = current_item,
+              assessment = "no_spaces",
+              source = current_source,
+              notes = NA_character_) %>% 
+      # add row for special chrs
+      add_row(pass_check = !has_special_chrs,
+              type = "file",
+              item = current_item,
+              assessment = "no_special_chrs",
+              source = current_source,
+              notes = paste("special characters:", special_characters, sep = " ")) %>% 
+      # add row for extensions
+      add_row(pass_check = non_proprietary,
+              type = "file",
+              item = current_item,
+              assessment = "non_proprietary",
+              source = current_source,
+              notes = paste0("extension: ", current_extension))
+
+  }
   
+  # assess file checks
   
+  # initialize empty report
+  file_report <- data.frame(
+    pass_check = as.character(),
+    type = as.character(),
+    assessment = as.character(),
+    summary = as.character()
+  )
+ 
+  #  update for spaces
+  file_no_spaces_check <- file_checks %>%
+    filter(assessment == "no_spaces" & pass_check == FALSE)
   
+  if (nrow(file_no_spaces_check) > 0) {
+    
+    # update report with FAILED
+    file_report <- file_report %>% 
+      add_row(pass_check = "FAILED",
+              type = "file",
+              assessment = "no_spaces",
+              summary = paste0("Found spaces in ", nrow(file_no_spaces_check), " files: ", toString(file_no_spaces_check$item)))
+    
+    # add failed checks to master failed_checks df
+    failed_checks <- failed_checks %>% 
+      add_row(file_no_spaces_check)
+    
+    
+  } else if (nrow(file_no_spaces_check) == 0) {
+    
+    # update report with PASSED
+    file_report <- file_report %>% 
+      add_row(pass_check = "PASSED",
+              type = "file",
+              assessment = "no_spaces",
+              summary = paste0("There are no spaces in ", length(current_files), " files."))
+  }
   
+  # update for special chrs
+  file_no_special_chrs_check <- file_checks %>%
+    filter(assessment == "no_special_chrs" & pass_check == FALSE)
+  
+  if (nrow(file_no_special_chrs_check) > 0) {
+    
+    # update report with FAILED
+    file_report <- file_report %>% 
+      add_row(pass_check = "FAILED",
+              tyoe = "file",
+              assessment = "no_special_chrs",
+              summary = paste0("Found special characters in ", nrow(file_no_special_chrs_check), " folders: ", toString(file_no_special_chrs_check$item)))
+    
+    # add failed checks to master failed_checks df
+    failed_checks <- failed_checks %>% 
+      add_row(file_no_special_chrs_check)
+    
+  } else if (nrow(file_no_special_chrs_check) == 0) {
+    
+    # update report with PASSED
+    file_report <- file_report %>% 
+      add_row(pass_check = "PASSED",
+              type = "file",
+              assessment = "no_special_chrs",
+              summary = paste0("There are no special characters in ", length(current_files), " files."))
+  }
+  
+  # update for extensions
+  file_non_proprietary_check <- file_checks %>% 
+    filter(assessment == "non_proprietary" & pass_check == FALSE)
+  
+  if (nrow(file_non_proprietary_check) > 0) {
+    
+    # update report with FAILED
+    file_report <- file_report %>% 
+      add_row(pass_check = "FAILED",
+              tyoe = "file",
+              assessment = "non_proprietary",
+              summary = paste0("Found non-proprietary extensions in ", nrow(file_no_special_chrs_check), " files: ", toString(file_non_proprietary_check$item)))
+    
+    # add failed checks to master failed_checks df
+    failed_checks <- failed_checks %>% 
+      add_row(file_non_proprietary_check)
+    
+  } else if (nrow(file_non_proprietary_check) == 0) {
+    
+    # update report with PASSED
+    file_report <- file_report %>% 
+      add_row(pass_check = "PASSED",
+              type = "file",
+              assessment = "non_proprietary",
+              summary = paste0("There are no non-proprietary extensions in ", length(current_files), " files."))
+  }
+  
+  # add file_checks to output list
+  data_checks$data_checks$files <- file_checks
+  
+  log_info("File check complete.")
+
   
   ### Clean up output ##########################################################
   
-  print(data_checks$report %>% 
-    select(pass_check, type, assessment))
+  # add report to output list
+  data_checks$report <- bind_rows(list(folder_report, file_report)) %>% 
+    as.data.frame()
   
+  # add failed_checks to output list
+  data_checks$failed_checks <- failed_checks
+  
+  report_summary <- data_checks$report %>% 
+    select(pass_check, type, assessment)
+  
+  print(report_summary)
+  
+  log_info("Copying report to clipboard.")
+  
+  write_clip(data_checks$report)
+  
+  log_info("check_files complete")
   return(data_checks)
   
 }
-
-
-
-
-
-
-# can you copy report to clipboard?
-
-
 
