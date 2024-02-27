2da2d5751e2a8e2585093baa1fc1c21fcbb75d72
BP <66450457+bpowers24@users.noreply.github.com>
2024-02-26 17:02:24 -0800

    Create create_directory_tree.R

---
+++ Data_Transformation/functions/create_directory_tree.R
@@ -0,0 +1,12 @@
+### create_directory_tree.R #############
+# Date created: 2024-02-26
+# Author: Bibi Powers-McCormack
+
+# Objective: 
+
+
+###
+
+create_directory_tree ← function(directory, output = "") {
+
+} 
