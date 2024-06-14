ccb2f8df5d7dd34c8db16171d274132549dad71b
BP <66450457+bpowers24@users.noreply.github.com>
2024-06-14 11:03:53 -0700

    initial commit for html checks report

--- Data_Package_Validation/data_package_checks.R
+++ Data_Package_Validation/data_package_checks.R
@@ -7,7 +7,10 @@
 # Directions: Fill out the user inputs. Then run the chunk.
 
 # data package directory (do not include a "/" at the end)
-directory <- "Z:/00_Cross-SFA_ESSDIVE-Data-Package-Upload/03_Manuscript-Data-Package-Folders/A/2024_BSLE_Processed_OM_Manuscripts_Data_Package" # note: parent folder "2024_BSLE_Processed_OM_Manuscripts_Data_Package" was temporarily renamed "A" because the file path was too long
+directory <- "Z:/00_Cross-SFA_ESSDIVE-Data-Package-Upload/03_Manuscript-Data-Package-Folders/00_ARCHIVE-WHEN-PUBLISHED/Kassianov_2023_AML_Plumes_Manuscript_Data_Package"
+
+# provide the name of the person running the checks
+report_author <- "Bibi Powers-McCormack"
 
 
 ### Prep Script ################################################################
@@ -20,6 +23,10 @@ library(rlog)
 library(fs) # for tree diagram
 library(clipr) # for copying to clipboard
 library(knitr) # for kable
+library(kableExtra) # for rmd report table styling
+library(DT) # for interactive tables in report
+library(rmarkdown) # for rendering report
+library(plotly) # for interactive graphs
 
 # set working directory to this GitHub repo (rcsfa-data-processing-for-publication)
 current_path <- rstudioapi::getActiveDocumentContext()$path # get current path
@@ -47,4 +54,9 @@ files_check <- check_files(directory)
 
 # 3. Check tabular data
 tabular_data_check <- check_tabular_data(data_package_data)
+
+
+# 4. Generate report
+render("./Data_Package_Validation/functions/checks_report.Rmd", output_format = "html_document")
+browseURL("./Data_Package_Validation/functions/checks_report.html")
 
