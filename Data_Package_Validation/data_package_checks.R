ecda7bb5b26aba82fc87f191183426f1fc2777a9
BP <66450457+bpowers24@users.noreply.github.com>
2024-08-07 12:14:02 -0700

    Update data_package_checks.R
    begin to refactor this to incorporate the new checks that are still in development

--- Data_Package_Validation/data_package_checks.R
+++ Data_Package_Validation/data_package_checks.R
@@ -1,13 +1,13 @@
 ### data_package_checks.R ######################################################
 
-# Objective: 
+# Objective: Use this script to run all data package checks.
 
 
 ### User Inputs ################################################################
 # Directions: Fill out the user inputs. Then run the chunk.
 
 # data package directory (do not include a "/" at the end)
-directory <- "Z:/00_Cross-SFA_ESSDIVE-Data-Package-Upload/03_Manuscript-Data-Package-Folders/00_ARCHIVE-WHEN-PUBLISHED/Kassianov_2023_AML_Plumes_Manuscript_Data_Package"
+directory <- "Z:/00_Cross-SFA_ESSDIVE-Data-Package-Upload/03_Manuscript-Data-Package-Folders/00_ARCHIVE-WHEN-PUBLISHED/Kassianov_2023_AML_Plumes_Manuscript_Data_Package/Kassianov_2023_AML_Plumes"
 
 # provide the name of the person running the checks
 report_author <- "Bibi Powers-McCormack"
@@ -20,6 +20,7 @@ report_author <- "Bibi Powers-McCormack"
 library(rstudioapi)
 library(tidyverse)
 library(rlog)
+library(devtools) # for sourcing from github
 library(fs) # for tree diagram
 library(clipr) # for copying to clipboard
 library(knitr) # for kable
@@ -36,24 +37,25 @@ setwd("../...") # move wd back to the repo directory
 getwd()
 
 # load functions
-source("./Data_Transformation/functions/load_tabular_data.R")
-source("./Data_Package_Validation/functions/check_files.R")
-source("./Data_Package_Validation/functions/check_tabular_data.R")
-
+source_url("https://raw.githubusercontent.com/river-corridors-sfa/rcsfa-data_processing_for_publication/database_v2/Data_Transformation/functions/load_tabular_data_from_flmd.R")
+source_url("https://raw.githubusercontent.com/river-corridors-sfa/rcsfa-data_processing_for_publication/database_v2/Data_Package_Documentation/functions/create_flmd_skeleton_v2.R")
 
 ### Run Functions ##############################################################
 # Directions: Run this chunk without modification. Answer inline prompts as they appear.
 
-# 1. Load data
-data_package_data <- load_tabular_data(directory)
+# 1. Load flmd
+data_package_flmd <- create_flmd_skeleton(directory = directory) %>% 
+  # convert to R's NA
+  mutate(across(everything(), ~ case_when(. == -9999 ~ NA, 
+                                          . == "N/A" ~ NA,
+                                          TRUE ~ .)))
+
+# 2. Load data
+data_package_data <- load_tabular_data_from_flmd(directory = directory, flmd_df = data_package_flmd)
 
 
-# 2. Check files
-files_check <- check_files(directory)
+# 3. Run checks
 
-
-# 3. Check tabular data
-tabular_data_check <- check_tabular_data(data_package_data)
 
 
 # 4. Generate report
