fa4a928d2a20116948b6e256d2fa3c9737a078a6
BP <66450457+bpowers24@users.noreply.github.com>
2024-10-29 16:10:41 -0700

    set up test driven development

---
+++ Data_Transformation/functions/test-rename_and_copy_folders.R
@@ -0,0 +1,125 @@
+### test-rename_and_copy_folders.R #############################################
+# Date Created: 2024-10-29
+# Date Updated: 2024-10-29
+# Author: Bibi Powers-McCormack
+
+# this script generates test data and then uses it to check the functionality of
+# the rename_and_copy_folder.R function
+
+
+### Prep script ################################################################
+
+# load libraries
+library(tidyverse)
+library(devtools)
+library(testthat)
+library(rlog)
+
+### Create testing data ########################################################
+
+# create testing source data
+temp_directory <- tempdir()
+log_info(paste0("Opening temp directory: ", temp_directory))
+shell.exec(temp_directory)
+
+# create source sub folders
+temp_source_folder <- paste0(temp_directory, "/source")
+temp_destination_folder <- paste0(temp_directory, "/destination")
+
+if (dir.exists(temp_source_folder)) {
+  # if source folder exists, remove it
+  unlink(temp_source_folder, recursive = T)
+}
+
+if (dir.exists(temp_destination_folder)) {
+  # if destination folder exists, remove it
+  unlink(temp_destination_folder, recursive = T)
+}
+
+dir.create(temp_source_folder)
+
+# create source sample files in a nested format
+temp_sample_names <- c("A", "B", "C", "D")
+
+for (sample in temp_sample_names) {
+  
+  # create folder for each sample
+  current_temp_folder <- paste0(temp_source_folder,"/Sample", sample)
+  dir.create(file.path(current_temp_folder))
+  
+  # create files for each sample
+  for (rep in 1:3) {
+    
+    current_temp_file_path <- paste0(current_temp_folder, "/sample", sample, "-", rep, ".csv")
+    file.create(current_temp_file_path)
+  } # end of loop to create files for each sample
+} # end of loop to create sample folders
+
+# create additional source files and folders
+dir.create(paste0(temp_source_folder, "/Sample123"))
+dir.create(paste0(temp_source_folder, "/Sample123456"))
+file.create(paste0(temp_source_folder, "/Sample123/SampleE.md"))
+
+
+
+# create "correct" files in a temp destination folder
+temp_destination_folder <- paste0(temp_directory, "/destination")
+dir.create(temp_destination_folder)
+
+# create sample files in a nested format
+temp_sample_names <- c("A", "B", "C")
+
+# create destination sample files in a nested format
+for (i in seq_along(temp_sample_names)) {
+  
+  # create folder for each sample
+  current_temp_folder <- paste0(temp_destination_folder,"/Sample", i)
+  dir.create(file.path(current_temp_folder))
+  
+  # create files for each sample
+  for (rep in 1:3) {
+    
+    current_temp_file_path <- paste0(current_temp_folder, "/sample", temp_sample_names[i], "-", rep, ".csv")
+    file.create(current_temp_file_path)
+  }
+}
+
+
+
+
+### Run tests ##################################################################
+
+test_that("files in the destination folder match the names of the files in the lookup df", {
+  
+  # create testing input lookup df
+  test_lookup <- tibble(source = c(paste0(temp_directory, "/source/SampleA"),
+                                   paste0(temp_directory, "/source/SampleB"),
+                                   paste0(temp_directory, "/source/SampleC"),
+                                   paste0(temp_directory, "/source/SampleD"),
+                                   paste0(temp_directory, "/source/Sample123")),
+                        destination = c(paste0(temp_directory, "/destination/Sample1"),
+                                        paste0(temp_directory, "/destination/Sample2"),
+                                        paste0(temp_directory, "/destination/Sample3"),
+                                        paste0(temp_directory, "/destination/Sample4"),
+                                        paste0(temp_directory, "/destination/SampleABC")))
+  test_lookup
+  
+  
+  # run function
+  rename_and_copy_folders(test_lookup)
+  
+  # get output of function
+  function_output <- list.dirs(paste0(temp_directory, "/destination"), full.names = T, recursive = T)[-1] # gets dirs and removes the first one (which is the parent dir)
+  
+  # test that the sub folders in the destination directory match the folder names listed in the lookup df
+  expect_equal(function_output, test_lookup$destination)
+  
+  # test that destination file names still equal the source file names (making sure that the file names weren't renamed)
+  expect_equal(sort(basename(list.files(paste0(temp_directory, "/destination"), recursive = T))), 
+               sort(basename(list.files(paste0(temp_directory, "/source"), recursive = T))))
+  
+})
+
+
+
+
