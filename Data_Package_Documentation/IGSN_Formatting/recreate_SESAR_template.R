dc5f91f565f6d814a53135c7711ccdc06ea9fa24
BP <66450457+bpowers24@users.noreply.github.com>
2024-11-08 09:37:52 -0800

    Update recreate_SESAR_template.R
    add checks

--- Data_Package_Documentation/IGSN_Formatting/recreate_SESAR_template.R
+++ Data_Package_Documentation/IGSN_Formatting/recreate_SESAR_template.R
@@ -1,6 +1,6 @@
 ### recreate_SESAR_template.R ##################################################
 # Date Created: 2024-11-07
-# Date Updated: 2024-11-07
+# Date Updated: 2024-11-08
 # Author: Bibi Powers-McCormack
 
 # Background: 
@@ -42,6 +42,7 @@ library(devtools)
 library(rlog)
 library(clipr)
 library(readxl)
+library(testthat)
 
 # load functions
 source_url("https://raw.githubusercontent.com/river-corridors-sfa/rcsfa-data_processing_for_publication/refs/heads/main/Data_Transformation/functions/rename_column_headers.R")
@@ -124,7 +125,7 @@ write_clip(paste0(temp_dir, "/samples"))
 output_site_header <- tibble('Object Type:'= as.character(),
                             'Site'= as.character(),
                             'User Code:'= as.character(), 
-                            'IEWDR' = as.character()) # 'IEWDR' is for WHONDRS
+                            'IEPRS' = as.character()) # 'IEWDR' is for WHONDRS  'IEPRS' is not for WHONDRS
 
 output_site_columns <- c("Sample Name", "IGSN", "Parent IGSN", 
                          "Release Date", "Geological unit", "Comment", 
@@ -137,7 +138,7 @@ output_site_columns <- c("Sample Name", "IGSN", "Parent IGSN",
 output_sample_header <- tibble('Object Type:'= as.character(),
                             'Individual Sample'= as.character(),
                             'User Code:'= as.character(), 
-                            'IEWDR' = as.character()) # 'IEWDR' is for WHONDRS
+                            'IEPRS' = as.character()) # 'IEWDR' is for WHONDRS  'IEPRS' is not for WHONDRS
 
 output_sample_columns <- c("Sample Name", "IGSN", "Parent IGSN", "Release Date", 
                            "Material", "Field name (informal classification)", "Classification", 
@@ -229,6 +230,68 @@ output_samples_template <- samples_template %>%
          `Collector/Chief Scientist`, `Collection date`, `Collection method`, `Collection method description`, `Field program/cruise`,
          Latitude, Longitude, `Primary physiographic feature`, Country,
          `Release Date`, `Current Registrant Name`, `Original Registrant Name`, URL)
+
+
+### Check for ESS-DIVE required columns ########################################
+# checks that the required columns (according to https://ess-dive.gitbook.io/sample-id-and-metadata/guide) are present
+
+test_that("Header rows are correct", {
+  
+  # check samples
+  # first 3 cols are Object Type:, Individual Sample, User Code:
+  # 4th col is either IEWDR or IEPRS
+  expect_equal(colnames(output_sample_header)[1:3], c("Object Type:", "Individual Sample", "User Code:"))
+  expect_true(colnames(output_sample_header[4]) %in% c("IEWDR", "IEPRS")) # 'IEWDR' is for WHONDRS  'IEPRS' is not for WHONDRS
+  
+  # check sites
+  # first 3 cols are Object Type:, Site, User Code:
+  # 4th col is either IEWDR or IEPRS
+  expect_equal(colnames(output_site_header)[1:3], c("Object Type:", "Site", "User Code:"))
+  expect_true(colnames(output_site_header[4]) %in% c("IEWDR", "IEPRS")) # 'IEWDR' is for WHONDRS  'IEPRS' is not for WHONDRS
+  
+})
+
+
+test_that("Required SITES columns are present", {
+  
+  required_cols <- c("Sample Name", "Material", "Collector/Chief Scientist", 
+                     "Collection date", "Collection method description", "Field program/cruise",
+                     "Latitude", "Longitude", "Primary physiographic feature", "Release Date")
+  
+  expect_true(all(required_cols %in% colnames(output_samples_template)))
+  
+  cat("Required cols NOT in your template: ")
+  cat("\n")
+  print(setdiff(required_cols, colnames(output_samples_template)))
+  cat("\n")
+  cat("Non-required cols in your template: ")
+  cat("\n")
+  print(setdiff(colnames(output_samples_template), required_cols))
+  cat("\n")
+  cat("\n")
+
+})
+
+
+test_that("Required SAMPLES columns are present", {
+  
+  required_cols <- c("Sample Name", "Material", "Collector/Chief Scientist", 
+                     "Collection date", "Collection method description", "Field program/cruise",
+                     "Latitude", "Longitude", "Primary physiographic feature", "Release Date")
+  
+  expect_true(all(required_cols %in% colnames(output_sites_template)))
+  
+  cat("Required cols NOT in your template: ")
+  cat("\n")
+  print(setdiff(required_cols, colnames(output_sites_template)))
+  cat("\n")
+  cat("Non-required cols in your template: ")
+  cat("\n")
+  print(setdiff(colnames(output_sites_template), required_cols))
+  cat("\n")
+  cat("\n")
+  
+})
 
 
 ### Create readme for ESS_DIVE_Infrastructure_ONLY folder ######################
