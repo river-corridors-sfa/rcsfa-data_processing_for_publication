0ccb8f84a4f81634e1f9f9db026fd82f9f1f5473
BP <66450457+bpowers24@users.noreply.github.com>
2025-04-03 15:26:36 -0700

    Update Regier_2024_WRB_YRB_Scaling_DP_Prep.R

--- Study_Data_Package_Specific_Scripts/Regier_2024_WRB_YRB_Scaling_DP_Prep.R
+++ Study_Data_Package_Specific_Scripts/Regier_2024_WRB_YRB_Scaling_DP_Prep.R
@@ -271,3 +271,48 @@ write_csv(dd, paste0(out_directory, "/Regier_2024_WRB_YRB_Scaling/Regier_2024_WR
 shell.exec(out_directory) # on windows
 system(paste0("open '", out_directory, "'")) # on mac
 
+
+### check authors ##############################################################
+# load libraries
+library(tidyverse)
+library(rlog)
+library(officer) # for reading in docx files
+library(readxl) # for reading in excel files
+library(janitor) # for cleaning up col headers
+library(glue)
+library(devtools) # for sourcing in script
+library(jsonlite) # for converting to json-ld file
+library(httr) # for uploading to the API
+library(clipr)
+
+# load functions
+source_url("https://raw.githubusercontent.com/river-corridors-sfa/rcsfa-data_processing_for_publication/refs/heads/main/Data_Transformation/functions/rename_column_headers.R")
+source_url("https://raw.githubusercontent.com/river-corridors-sfa/rcsfa-data_processing_for_publication/refs/heads/main/Data_Package_ESS-DIVE/update_ESS-DIVE_landing_page/functions/update_landing_page_authors.R")
+source_url("https://raw.githubusercontent.com/river-corridors-sfa/rcsfa-data_processing_for_publication/refs/heads/main/Data_Package_ESS-DIVE/update_ESS-DIVE_landing_page/functions/update_landing_page_coordinates.R")
+
+
+# USER INPUTS
+your_essdive_metadata_file <- "Z:/00_ESSDIVE/03_Manuscript_DPs/Regier_2024_WRB_YRB_Scaling_Manuscript_Data_Package/ESSDIVE_Metadata.docx" # absolute file path of ESS-DIVE metadata .docx
+your_author_spreadsheet <- "Z:/00_ESSDIVE/00_Instructions/RC_SFA_author_information.xlsx"
+your_api_token = "" # this is your personal token that you can get after signing into ess-dive; recommend adding this in the console
+your_essdive_id = "" # id that begins with "ess-dive-" found on the landing page you want to update
+your_upload_site = "main" # options: c("sandbox", "main")
+
+
+# RUN functions (no modifications needed)
+# get authors from ESS-DIVE metadata file
+author_names <- get_authors_from_essdive_metadata(essdive_metadata_file = your_essdive_metadata_file)
+
+
+# get author info from spreadsheet
+author_info <- get_author_spreadsheet_info(author_df = author_names, 
+                                           author_info_file = your_author_spreadsheet)
+
+write_clip(author_info)
+read_clip()
+
+# # update landing page - warning this will overwrite all existing authors with the new ones you provide
+# update_landing_page_authors(api_token = your_api_token,
+#                             author_df = author_info,
+#                             essdive_id = your_essdive_id,
+#                             upload_site = your_upload_site)
