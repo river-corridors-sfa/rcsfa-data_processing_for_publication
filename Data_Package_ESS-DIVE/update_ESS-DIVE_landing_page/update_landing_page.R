d1cdfc15d397e581405b46f506f94fc04705731f
BP <66450457+bpowers24@users.noreply.github.com>
2025-04-03 15:26:19 -0700

    upload authors for arton_2025_Coastal_Fires_Levo

--- Data_Package_ESS-DIVE/update_ESS-DIVE_landing_page/update_landing_page.R
+++ Data_Package_ESS-DIVE/update_ESS-DIVE_landing_page/update_landing_page.R
@@ -31,10 +31,10 @@ source_url("https://raw.githubusercontent.com/river-corridors-sfa/rcsfa-data_pro
 
 
 # USER INPUTS
-your_essdive_metadata_file <- "Z:/00_ESSDIVE/01_Study_DPs/WHONDRS_AV1_Data_Package/WHONDRS_AV1_ESSDIVE_Metadata.docx" # absolute file path of ESS-DIVE metadata .docx
+your_essdive_metadata_file <- "C:/Users/powe419/OneDrive - PNNL/Documents - RC-SFA/Data Management and Publishing/Data-Publishing/Manuscript-Data-Package/Files-for-review/Barton_2025_Coastal_Fires_Levo/ESSDIVE_Metadata_Template.docx" # absolute file path of ESS-DIVE metadata .docx
 your_author_spreadsheet <- "Z:/00_ESSDIVE/00_Instructions/RC_SFA_author_information.xlsx"
 your_api_token = "" # this is your personal token that you can get after signing into ess-dive; recommend adding this in the console
-your_essdive_id = "ess-dive-71ed0030c185c5a-20250307T191734493" # id that begins with "ess-dive-" found on the landing page you want to update
+your_essdive_id = "ess-dive-8c97b34a948d06e-20250331T200016582" # id that begins with "ess-dive-" found on the landing page you want to update
 your_upload_site = "main" # options: c("sandbox", "main")
 
 
