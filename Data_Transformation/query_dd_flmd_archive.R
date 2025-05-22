1095a001e147954ed15f32af15e1b837ed67d0ee
brieanne-forbes <brieanne.forbes@pnnl.gov>
2025-05-22 16:01:57 -0700

    Create query_dd_flmd_archive.R

---
+++ Data_Transformation/query_dd_flmd_archive.R
@@ -0,0 +1,41 @@
+# ==============================================================================
+#
+# Query archived dps to get list of flmd and dds to populate database with 
+# 
+# Bibi will add publish date manually and use as an input to the function 
+#
+# ==============================================================================
+#
+# Brieanne Forbes
+# 22 May 2025
+#
+# ==============================================================================
+
+library(tidyverse)
+
+rm(list=ls(all=T))
+
+# ================================= User inputs ================================
+
+study_dir <- 'Z:/00_ESSDIVE/01_Study_DPs/00_ARCHIVE-WHEN-PUBLISHED/WHONDRS_WROL2019_Data_Package_v2'
+
+manuscript_dir <- 'Z:/00_ESSDIVE/03_Manuscript_DPs/00_ARCHIVE-WHEN-PUBLISHED/2024_BSLE_Processed_OM_Manuscripts_Data_Package'
+
+outdir <- 'C:/Brieanne/GitHub/rcsfa-data_processing_for_publication/Data_Package_Documentation/database/All_dd_flmd_as_of_2025-05-22.csv'
+
+# ================================= find files ================================
+
+study_files <- list.files(study_dir, pattern = "(_dd\\.csv|_flmd\\.csv)$", full.names = T, recursive = T)
+
+manuscript_files <- list.files(manuscript_dir, pattern = "(_dd\\.csv|_flmd\\.csv)$", full.names = T, recursive = T)
+
+# ================================= remove unwanted files ======================
+
+all_files <-  tibble(archived_dd_flmd = c(study_files, manuscript_files))
+
+filtered <- all_files %>%
+  mutate(sans_dir = str_remove(archived_dd_flmd, study_dir),
+         sans_dir = str_remove(sans_dir, manuscript_dir)) %>%
+  filter(!str_detect(sans_dir, regex("archive", ignore_case = TRUE)))
+
+# write_csv(filtered, outdir)
