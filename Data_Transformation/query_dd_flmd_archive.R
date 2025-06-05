4b3b6bc78f1c6a1f11e2c25aef874456eaba0a57
brieanne-forbes <brieanne.forbes@pnnl.gov>
2025-06-05 14:49:33 -0700

    Update query_dd_flmd_archive.R
    remove manuscript dps, add additional ones to skip

--- Data_Transformation/query_dd_flmd_archive.R
+++ Data_Transformation/query_dd_flmd_archive.R
@@ -19,27 +19,25 @@ rm(list=ls(all=T))
 
 study_dir <- 'Z:/00_ESSDIVE/01_Study_DPs/00_ARCHIVE-WHEN-PUBLISHED'
 
-manuscript_dir <- 'Z:/00_ESSDIVE/03_Manuscript_DPs/00_ARCHIVE-WHEN-PUBLISHED'
-
 outdir <- 'C:/Users/forb086/Documents/GitHub/rcsfa-data_processing_for_publication/Data_Package_Documentation/database/All_dd_flmd_as_of_2025-05-22.csv'
 
 # ================================= find files ================================
 
 study_files <- list.files(study_dir, pattern = "(_dd\\.csv|_flmd\\.csv)$", full.names = T, recursive = T)
 
-manuscript_files <- list.files(manuscript_dir, pattern = "(_dd\\.csv|_flmd\\.csv)$", full.names = T, recursive = T)
-
 # ================================= remove unwanted files ======================
 
-all_files <-  tibble(archived_dd_flmd = c(study_files, manuscript_files))
 
-filtered <- all_files %>%
-  mutate(sans_dir = str_remove(archived_dd_flmd, study_dir),
-         sans_dir = str_remove(sans_dir, manuscript_dir)) %>%
+filtered <- study_files %>%
+  mutate(sans_dir = str_remove(archived_dd_flmd, study_dir)) %>%
   filter(!str_detect(sans_dir, regex("archive", ignore_case = TRUE))) %>%
   filter(!str_detect(sans_dir, 'prelim_dd.csv'))%>%
   filter(!str_detect(sans_dir, 'prelim_flmd.csv')) %>%
-  filter(sans_dir != '/SSS_Data_Package_v3/v2_SSS_dd.csv')
+  filter(sans_dir != '/SSS_Data_Package_v3/v2_SSS_dd.csv')%>%
+  filter(sans_dir != '/SSS_Data_Package_v3/v2_SSS_flmd.csv')%>%
+  filter(!str_detect(sans_dir, 'D50')) %>%
+  filter(sans_dir != '/RC2_TemporalStudy_2021-2022_SampleData_v3/v2_RC2_Sample_dd.csv') %>%
+  filter(sans_dir != '/RC2_TemporalStudy_2021-2022_SampleData_v3/v2_RC2_Sample_flmd.csv')
 
 anti <- anti_join(all_files, filtered)
 
