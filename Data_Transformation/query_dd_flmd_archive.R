2b65a3f60084f3eb9eaba55e40da1ab88f4c8880
Brieanne Forbes <108300528+brieanne-forbes@users.noreply.github.com>
2025-06-05 15:08:15 -0700

    Update query_dd_flmd_archive.R

--- Data_Transformation/query_dd_flmd_archive.R
+++ Data_Transformation/query_dd_flmd_archive.R
@@ -19,7 +19,7 @@ rm(list=ls(all=T))
 
 study_dir <- 'Z:/00_ESSDIVE/01_Study_DPs/00_ARCHIVE-WHEN-PUBLISHED'
 
-outdir <- 'C:/Users/forb086/Documents/GitHub/rcsfa-data_processing_for_publication/Data_Package_Documentation/database/All_dd_flmd_as_of_2025-05-22.csv'
+outdir <- 'C:/Users/forb086/Documents/GitHub/rcsfa-data_processing_for_publication/Data_Package_Documentation/database/All_dd_flmd_as_of_2025-06-05.csv'
 
 # ================================= find files ================================
 
@@ -27,8 +27,9 @@ study_files <- list.files(study_dir, pattern = "(_dd\\.csv|_flmd\\.csv)$", full.
 
 # ================================= remove unwanted files ======================
 
+all_files <-  tibble(archived_dd_flmd = study_files)
 
-filtered <- study_files %>%
+filtered <- all_files %>%
   mutate(sans_dir = str_remove(archived_dd_flmd, study_dir)) %>%
   filter(!str_detect(sans_dir, regex("archive", ignore_case = TRUE))) %>%
   filter(!str_detect(sans_dir, 'prelim_dd.csv'))%>%
