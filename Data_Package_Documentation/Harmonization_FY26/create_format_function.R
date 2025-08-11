472c4c973620eb6ca51cccd64e9b724ff8ad033c
brieanne-forbes <brieanne.forbes@pnnl.gov>
2025-08-11 12:19:25 -0700

    Create create_format_function.R

---
+++ Data_Package_Documentation/Harmonization_FY26/create_format_function.R
@@ -0,0 +1,19 @@
+# ==============================================================================
+#
+# create_format.R function for formatting a users data in the soil, sediment, 
+# water chemistry or hydrologic monitoring formats. 
+#
+# Status: in progress
+#
+# ==============================================================================
+#
+# Brieanne Forbes (brieanne.forbes@pnnl.gov)
+# 11 August 2025
+#
+# ==============================================================================
+
+library(tidyverse)
+
+rm(list=ls(all=T))
+
+# ================================ User Inputs =================================
