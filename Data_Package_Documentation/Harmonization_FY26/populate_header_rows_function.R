fda54461bdd0f5ea930c7a65cf39015f8110f5f7
brieanne-forbes <brieanne.forbes@pnnl.gov>
2025-08-15 15:18:38 -0700

    Create populate_header_rows_function.R

---
+++ Data_Package_Documentation/Harmonization_FY26/populate_header_rows_function.R
@@ -0,0 +1,44 @@
+#==============================================================================
+#
+# Function for populating the metadata headers in the soil, sediment, 
+# water chemistry or hydrologic monitoring formats. 
+#
+# Status: in progress
+#
+# ==============================================================================
+#
+# Brieanne Forbes (brieanne.forbes@pnnl.gov)
+# 17 August 2025
+#
+# ==============================================================================
+
+require(pacman)
+p_load(tidyverse,
+       rlog,
+       cli)
+
+# ================================ Documentation ===============================
+
+# This function populates the metadata headers rows to comply with ESS-DIVE soil, sediment, 
+# water chemistry, and hydrologic monitoring reporting formats (v2).
+
+# This function:
+# 1. 
+# 2.
+# 3. 
+# 4. 
+
+# Inputs: 
+# 
+
+# Outputs: 
+# 
+
+# Usage Examples:
+# 
+# ============================ populate_header_rows function ========================
+
+populate_header_rows <- function(data, 
+                                 header_row_input){
+  
+}
