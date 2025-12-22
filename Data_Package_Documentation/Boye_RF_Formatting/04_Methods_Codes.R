594f83a97dc7c5c281a7b95e24d005a76f138a45
brieanne-forbes <brieanne.forbes@pnnl.gov>
2025-12-22 15:52:35 -0800

    Update 04_Methods_Codes.R
    TAP

--- Data_Package_Documentation/Boye_RF_Formatting/04_Methods_Codes.R
+++ Data_Package_Documentation/Boye_RF_Formatting/04_Methods_Codes.R
@@ -24,12 +24,12 @@ dir <- 'C:/Users/forb086/OneDrive - PNNL/Data Generation and Files/'
 
 RC <- 'RC4'
 
-study_code <- 'YEP'
+study_code <- 'TAP'
 
 
 # ================================= Build dir ================================
 # boye_dir <- paste0(dir, RC, '/Boye_Files/', study_code, '/')
-boye_dir <- 'Z:/00_ESSDIVE/01_Study_DPs/YEP_Data_Package/YEP_Data_Package/YEP_Sample_Data'
+boye_dir <- 'Z:/00_ESSDIVE/01_Study_DPs/WHONDRS_TAP_Data_Package/WHONDRS_TAP_Data_Package/WHONDRS_TAP_Sample_Data'
 
 typical_codes <- paste0(dir, 'Workflows-MethodsCodes/Methods_Codes/Method_Typical_Codes.xlsx')
   
