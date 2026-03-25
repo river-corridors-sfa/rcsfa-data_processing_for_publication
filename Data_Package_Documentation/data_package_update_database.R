082300312af5be51ec7696a78179970cc9957b39
Brieanne Forbes <108300528+brieanne-forbes@users.noreply.github.com>
2026-03-25 15:53:56 -0700

    minikits

--- Data_Package_Documentation/data_package_update_database.R
+++ Data_Package_Documentation/data_package_update_database.R
@@ -12,10 +12,11 @@ my_publish_date <- Sys.Date()
 # my_publish_date <- '2025-07-21'
   
 # provide the absolute file path of the new DD to add
-my_dd <-"Z:/00_ESSDIVE/01_Study_DPs/00_ARCHIVE-WHEN-PUBLISHED/PRT_GameCamera_Data_Package/PRT_GameCamera_Data_Package/PRT_GameCamera_dd.csv"
+my_dd <-"Z:/00_ESSDIVE/01_Study_DPs/00_ARCHIVE-WHEN-PUBLISHED/WHONDRS_Minikits/WHONDRS_Minikits_Data_Package/WHONDRS_Minikits_dd.csv"
 
 # provide the absolute file path of the new FLMD to add
-my_flmd <- "Z:/00_ESSDIVE/01_Study_DPs/00_ARCHIVE-WHEN-PUBLISHED/PRT_GameCamera_Data_Package/PRT_GameCamera_Data_Package/PRT_GameCamera_flmd.csv"
+my_flmd <- "Z:/00_ESSDIVE/01_Study_DPs/00_ARCHIVE-WHEN-PUBLISHED/WHONDRS_Minikits/WHONDRS_Minikits_Data_Package/WHONDRS_Minikits_flmd.csv"
+
 
 # absolute file path of the DD database
 dd_database_dir <- "C:/Brieanne/GitHub/rcsfa-data_processing_for_publication/Data_Package_Documentation/database/data_dictionary_database.csv"
